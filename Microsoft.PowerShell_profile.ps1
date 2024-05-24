4bc9a1df738e920060edd971f4af4596a6b7628b
genchadt <62498018+genchadt@users.noreply.github.com>
2024-05-24 17:37:16 -0400

    Committing local changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -266,7 +266,7 @@ Set-PSReadLineOption -Colors @{
     String = 'DarkCyan'
 }
 
-### Final Line to set prompt
+## Final Line to set prompt
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/gruvbox.omp.json' | Invoke-Expression
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
