8fda7dcb1288a7c9c2461708016067e02706fcaf
genchadt <62498018+genchadt@users.noreply.github.com>
2024-12-21 19:02:48 -0500

    fix whitespace

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -81,13 +81,13 @@ function Test-CommandExists {
 
 # Editor Configuration
 $EDITOR = if (Test-CommandExists nvim) { 'nvim' }
-          elseif (Test-CommandExists pvim) { 'pvim' }
-          elseif (Test-CommandExists vim) { 'vim' }
-          elseif (Test-CommandExists vi) { 'vi' }
-          elseif (Test-CommandExists code) { 'code' }
-          elseif (Test-CommandExists notepad++) { 'notepad++' }
-          elseif (Test-CommandExists sublime_text) { 'sublime_text' }
-          else { 'notepad' }
+            elseif (Test-CommandExists pvim) { 'pvim' }
+            elseif (Test-CommandExists vim) { 'vim' }
+            elseif (Test-CommandExists vi) { 'vi' }
+            elseif (Test-CommandExists code) { 'code' }
+            elseif (Test-CommandExists notepad++) { 'notepad++' }
+            elseif (Test-CommandExists sublime_text) { 'sublime_text' }
+            else { 'notepad' }
 Set-Alias -Name vim -Value $EDITOR
 
 function Edit-Profile {
