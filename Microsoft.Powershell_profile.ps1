36a3bccda5aa1b541060e34809345cd0cc22d3dc
GenChadT <git+github@timothywb.com>
2025-05-29 22:50:20 -0400

    Remove zoxide installation logic

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -10,21 +10,6 @@ if (Get-Command ntop -ErrorAction SilentlyContinue) {
     Set-Alias -Name top -Value ntop
 }
 
-<# zoxide #>
-if (-not (Get-Command z -ErrorAction SilentlyContinue) -and -not (Get-Command zoxide -ErrorAction SilentlyContinue)) {
-    Write-Host "zoxide command not found. Attempting to install via winget..."
-    try {
-        winget install -e --id ajeetdsouza.zoxide
-        Write-Host "zoxide installed successfully. Initializing..."
-        Invoke-Expression (& { (zoxide init powershell | Out-String) })
-    } catch {
-        Write-Error "Failed to install zoxide. Error: $_"
-    }
-} elseif (Get-Command zoxide -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
-}
-#endregion
-
 #region Colors & Theming
 $PSReadLineOptions = @{
     Colors = @{
